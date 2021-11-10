844adaa098c00b8b40439bfb3f928516dd824214
Raif Harik <harik.raif@gmail.com>
2021-11-10 08:26:51 -0600

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+/*
+ * File: /.prettierrc.js
+ * Project: enumPlugin
+ * Created Date: 2021-11-08
+ * Author: Raif Harik
+ * 
+ */
+module.exports = {
+	useTabs: true,
+	tabWidth: 4,
+	semi: true,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	jsxBracketSameLine: true,
+};
